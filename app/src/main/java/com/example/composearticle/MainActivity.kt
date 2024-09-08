c4203f599ac071f96b17fb23ba96cf60ec31d128
Harmandeep01 <sharmandeep@gmail.com>
2024-09-08 13:21:37 +0530

    Corrected Code

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -1,21 +1,34 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.composearticle
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,17 +40,14 @@ import com.example.composearticle.ui.theme.ComposeArticleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ComposeArticleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ArticleUI(
-                        heading = stringResource( R.string.heading),
-                        text1 = stringResource( R.string.text1),
-                        text2 = stringResource(R.string.text2),
-                        modifier = Modifier.padding(innerPadding),
-
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticleApp()
                 }
             }
         }
@@ -45,77 +55,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Heading(heading: String, modifier: Modifier = Modifier) {
-Column(
-    modifier = modifier){
+fun ComposeArticleApp() {
+    ArticleCard(
+        title = stringResource(R.string.heading),
+        shortDescription = stringResource(R.string.compose_short_desc),
+        longDescription = stringResource(R.string.compose_long_desc),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+private fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
         Text(
-            text = heading,
-            fontSize = 24.sp,
-            modifier = modifier.padding(16.dp),
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
         )
-    }
-}
-
-@Composable
-fun TextField1(text1: String, modifier: Modifier = Modifier){
-    Column(
-        modifier = modifier
-    ){
         Text(
-            text = text1,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Justify,
-            modifier = modifier.padding(16.dp, 0.dp, 19.dp, 0.dp)
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
         )
-    }
-}
-
-@Composable
-fun TextField2(text2: String, modifier: Modifier = Modifier){
-    Column (horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier){
         Text(
-            text = text2,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Justify,
-            modifier = modifier.padding(16.dp)
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
         )
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun ArticleUI(heading: String, text1: String, text2: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.bg_compose_background)
-    Column (modifier = modifier){
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth
-        )
-        Heading(
-            heading,
-            modifier = modifier
-        )
-        TextField1(
-            text1,
-            modifier = modifier
-        )
-        TextField2(
-            text2 ,
-            modifier = modifier
-        )
-    }
-}
-
-@Preview(showBackground = true,
-    showSystemUi = true)
-@Composable
-fun ArticlePreview() {
+fun ComposeArticleAppPreview() {
     ComposeArticleTheme {
-        ArticleUI(
-            stringResource(R.string.heading),
-            stringResource(R.string.text1),
-            stringResource(R.string.text2)
-        )
+        ComposeArticleApp()
     }
 }
